bbc545f637171b2d88183520a9c8e8eb0bbbf755
Fransiscus Kevin <81698880+kvnsurya666@users.noreply.github.com>
2023-08-30 13:11:21 +0700

    betobe

---
+++ app/Views/home.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solusi Perdana Tehnik</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Feather Icons -->
+    <!-- choose one -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+</head>
+<body>
+    <!-- Navbar Start -->
+    <nav class="navbar">
+        <a href="#" class="navbar-logo"><img src="assets/img/logo-spt.jpg" alt="Logo Solusi Perdana Tehnik"></a>
+        <div class="navbar-nav">
+            <a href="#">Home    </a>
+            <a href="#about">About Us   </a>
+            <a href="#product">Our Products   </a>
+            
+        </div>
+        <div class="navbar-extra">
+            <a href="#" id="hamburger-menu"><i data-feather=menu></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Hero Section Start -->
+    <section class="hero" id="home">
+        
+    <main class="content">
+        <h1>Selamat Datang di <span>Solusi Perdana Tehnik</span></h1>
+        <p>
+          Kami menyediakan berbagai macam produk kimia untuk melindungi logam dan mesin anda.
+        </p>
+    </main>
+        
+    </section>
+    <!-- Hero Section End -->
+
+    <!-- About Section Start -->
+    <section id="about" class="about">
+      <h2><span>Tentang</span> Kami</h2>
+      <div class="row">
+        <div class="about-img">
+          <img src="assets/img/gambar-1.png" alt="Tentang Kami" />
+        </div>
+        <div class="content">
+          <h3>Mengapa memilih produk kami?</h3>
+          <p>
+            Kami menjual produk - produk kimia yang berkualitas dan harga terjangkau.
+          </p>
+          <p>
+            Produk - produk kami akan melindungi logam dan mesin anda dari karat, keropos, dan laian - lain.
+          </p>
+          <p>
+            Kami juga menjual produk pengharum ruangan yang akan membuat anda nyaman dan relax.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!-- About Section End -->
+
+    <!-- Product Section Start -->
+    <section id="product" class="product">
+      <h2><span>Produk</span> Kami</h2>
+      <p>Berikut ini adalah produk - produk kami.</p>
+      <div class="row">
+        <div class="product-card">
+          <img src="assets/img/products/Rust Remover.png" alt="Rust Remover" />
+          <h3 class="product-card-title">- BETOBE Rust Remover -</h3>
+          
+        </div>
+
+        <div class="product-card">
+          <img src="assets/img/products/Rust Penetrant.png" alt="Rust Penetrant" />
+          <h3 class="product-card-title">- BETOBE Rust Peneterant -</h3>
+          
+        </div>
+
+        <div class="product-card">
+          <img src="assets/img/products/Anti Seize Molybdenum.png" alt="Anti Seize Molybdenum" />
+          <h3 class="product-card-title">- BETOBE Anti Seize Molybdenum -</h3>
+          
+        </div>
+
+        <div class="product-card">
+          <img
+            src="assets/img/products/Rubberized Undercoat.png"
+            alt="Rubberized Undercoat"
+          />
+          <h3 class="product-card-title">- BETOBE Rubberized Undercoat -</h3>
+          
+        </div>
+
+        <div class="product-card">
+          <img src="assets/img/products/Electrical Switch and Contact Cleaner.png" alt="Anti Seize Molybdenum" />
+          <h3 class="product-card-title">- BETOBE Electrical Switch and Contact Cleaner -</h3>
+          
+        </div>
+
+        <div class="product-card">
+          <img src="assets/img/products/Penetrating Oil.png" alt="Anti Seize Molybdenum" />
+          <h3 class="product-card-title">- BETOBE Penetrating Oil -</h3>
+          
+        </div>
+        
+      </div>
+      <div class="row">
+            <a href="https://shopee.co.id/kevinsuryaa21" class="button">Beli Sekarang</a>
+      </div>
+    </section>
+    <!-- Product Section end -->
+
+    <!-- Footer start -->
+    <footer>
+      <div class="socials">
+        <a href="https://www.instagram.com/betobechem/"
+          ><i data-feather="instagram"></i
+        ></a>
+        <a href="https://api.whatsapp.com/send?phone=6285655131099&amp;text=Halo Bapak/Ibu, Saya dapat nomor dari www.solusiperdanatehnik.com"><i data-feather="phone"></i></a>
+        
+      </div>
+      <div class="links">
+        <a href="#">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#product">Menu</a>
+        
+      </div>
+      <div class="credit">
+        <p>
+          Created by
+          <a href="https://www.instagram.com/fkvnsurya/">Fransiscus Kevin</a>. |
+          &copy; 2023.
+        </p>
+      </div>
+    </footer>
+    <!-- Footer end -->
+
+    <!-- Feather Icons -->
+    <script>
+      feather.replace();
+    </script>
+
+    <!-- My Javascript -->
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
